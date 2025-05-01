c0a554a7eadda31db8db564b0f5ed823c0050b5b
Krusto <k.stoianov2@gmail.com>
2025-05-01 13:49:26 +0300

    removed not used global variables

--- EngineLib/Renderer/Vulkan/VulkanRendererAPI.cpp
+++ EngineLib/Renderer/Vulkan/VulkanRendererAPI.cpp
@@ -91,8 +91,6 @@ namespace LunaraEngine
         std::vector<VkPresentModeKHR> presentModes;
     };
 
-    const std::array<const char*, 1> g_SwapChainExtensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
-
     void VulkanRendererAPI::CreateWindow()
     {
         SDL_Init(SDL_INIT_VIDEO);
